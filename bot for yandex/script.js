f15863b117c6a79eec3f5d19ffb2f23206512699
kremlina <71058945+kremlina@users.noreply.github.com>
2020-09-16 14:17:11 +0300

    yandex1
    1) Умеет выбирать случайное слово из массива 
    2) Писать его в поисковике яндекс
    3)Кликает по ссылке и совершает переход

---
+++ bot for yandex/script.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Bot for Yandex
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://yandex.ru/*
+// @grant        none
+// ==/UserScript==
+
+let keywords = ['Гобой','Как звучит флейта','Кларнет'];
+let keyword = keywords[getRandom(0,keywords.length)];
+let buttonYA = document.getElementsByTagName(".button_theme_websearch")[0];
+
+if (buttonYA != 0){
+    document.querySelectorAll('.button_theme_websearch')[0].click();
+    document.getElementsByName('text')[0].value = keyword;
+}else{
+
+    let links = document.links;
+    for(let i=0; i<links.length; i++){
+        if(links[i].href.indexOf('xn----7sbab5aqcbiddtdj1e1g.xn--p1ai') != -1){
+           links[i].removeAttribute("target");
+            console.log('Ссылка найдена '+links[i]);
+            links[i].click();
+        }
+    }
+}
+function getRandom(min,max){
+    return Math.floor(Math.random()*(max-min)+min);
+}
